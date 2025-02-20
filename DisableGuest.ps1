ff46bc2625b1d6db54d3bbeeaf0b62be0412b5f5
Cryptoke1 <147792783+Cryptoke1@users.noreply.github.com>
2025-02-19 23:17:27 -0500

    Delete DisableGuest.ps1

--- DisableGuest.ps1
+++
@@ -1,8 +0,0 @@
-# Disable the Guest account
-$Guest = Get-LocalUser -Name Guest -ErrorAction SilentlyContinue
-if($Guest){
-    Disable-LocalUser -Name Guest
-    Write-Host "Guest account disabled."
-} else {
-    Write-Host "Guest account not found."
-}
